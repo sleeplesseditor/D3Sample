5164355f571fbf2aefc89ffa6183495c8065602b
sleeplesseditor <josh@bryson.me.uk>
2018-04-17 15:06:11 +0100

    Bar Graph axis setup

--- script.js
+++ script.js
@@ -1,40 +1,31 @@
-var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+var data = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-var svgWidth = 500, svgHeight = 300, barPadding = 5;
-var barWidth = (svgWidth / dataset.length);
+var svgWidth = 500, svgHeight = 300;
 
 var svg = d3.select('svg')
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
-var barChart = svg.selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-    .attr("y", function(d) {
-        return svgHeight - d
-    })
-    .attr("height", function(d) {
-        return d;
-    })
-    .attr("width", barWidth - barPadding)
-    .attr("class", "bar")
-    .attr("transform", function (d, i) {
-        var translate = [barWidth * i, 0];
-        return "translate("+ translate +")";
-    });
+var xScale = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, svgWidth]);
 
-var text = svg.selectAll("text")
-    .data(dataset)
-    .enter()
-    .append("text")
-    .text(function(d) {
-        return d;
-    })
-    .attr("y", function (d, i) {
-        return svgHeight - d - 2;
-    })
-    .attr("x", function(d, i) {
-        return barWidth * i;
-    })
-    .attr("fill", "#A64C38");
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, svgHeight]);
+
+var x_axis = d3.axisBottom()
+    .scale(xScale);
+
+var y_axis = d3.axisLeft()
+    .scale(yScale);
+
+svg.append("g")
+    .attr("transform", "translate(50, 10)")
+    .call(y_axis);
+
+var xAxisTranslate = svgHeight - 20;
+
+svg.append("g")
+    .attr("transform", "translate(50, " + xAxisTranslate +")")
+    .call(x_axis);
